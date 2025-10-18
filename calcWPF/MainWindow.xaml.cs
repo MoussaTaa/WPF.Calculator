d8006b91aa45e4015efafd7dadf2f437f86bdf0f
Moussa Taaloucht <moussataaloucht0000@gmail.com>
2025-10-18 21:18:39 +0100

    Complait the calculator logics and events

--- calcWPF/MainWindow.xaml.cs
+++ calcWPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Linq.Expressions;
+using System.Security.Permissions;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,11 +14,11 @@ using System.Windows.Shapes;
 
 namespace calcWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        string equation = "";
+        double result;
         public MainWindow()
         {
             InitializeComponent();
@@ -25,13 +28,134 @@ namespace calcWPF
         {
             Application.Current.Shutdown();
         }
-
         private void Window_Activated(object sender, EventArgs e)
         {
             this.MouseDown += delegate
             {
                 DragMove();
             };
+        }
+
+        public void addToEquation(string value)
+        {
+            equation += value;
+            lblEquation.Content = equation;
+        }
+        public void addOperatorToEquation(string op) {
+            if (equation == "")
+            {
+                addToEquation(result.ToString());
+                addToEquation(op);
+            }
+            else if (containOperator())
+            {
+
+                equation = equation.Remove(equation.Length - 1);
+                addToEquation(op);
+            }
+            else
+            {
+                addToEquation(op);
+            }
+        }
+        public bool containOperator() 
+        {
+            bool isContainOperator = false;
+            char lastChar = equation[equation.Length - 1];
+            if (lastChar == '+' || lastChar == '*' || lastChar == '-' || lastChar == '/' )
+            {
+                isContainOperator = true;
+            }
+            return isContainOperator;
+        }
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("0");
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("1");
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("2");
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("3");
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("4");
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("5");
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("6");
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("7");
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("8");
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation("9");
+        }
+
+        private void btnPoint_Click(object sender, RoutedEventArgs e)
+        {
+            addToEquation(".");
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            addOperatorToEquation("+");
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            addOperatorToEquation("-");
+        }
+
+        private void btnMultiplication_Click(object sender, RoutedEventArgs e)
+        {
+            addOperatorToEquation("*");
+        }
+
+        private void btnDivision_Click(object sender, RoutedEventArgs e)
+        {
+            addOperatorToEquation("/");
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            equation = "";
+            result = 0;
+            lblEquation.Content = "";
+            lblResult.Content = "0";
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            var equationToResult = new DataTable().Compute(equation, null);
+            result = Convert.ToDouble(equationToResult);
+            lblResult.Content = result.ToString();
         }
     }
 }
